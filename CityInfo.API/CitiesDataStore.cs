3a5a20e5af4ef3c744a05caf903d14b6c5cf2e2d
Jesse Varis <jessevaris@hotmail.com>
2022-03-14 00:07:03 +0100

    PointOfInterestDto

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,22 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            // Init dummy data
+            Cities = new List<CityDto>()
+            {
+                new CityDto() { Id = 1, Name ="New York City", Description = "The bla bla..."},
+                new CityDto() { Id = 2, Name ="Antwerp", Description = "Bla bla..."},
+                new CityDto(){ Id = 3, Name = "Paris", Description = "Voilá."}
+
+            };
+        }
+    }
+}
